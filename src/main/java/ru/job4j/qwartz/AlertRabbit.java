00acb7adeab1ed3dc8a2e6700001ff11e5983cdb
voldomarich <vbekasov1987@gmail.com>
2024-02-19 19:51:42 +0300

    1.1. Job c параметрами [#260360]

--- src/main/java/ru/job4j/qwartz/AlertRabbit.java
+++ src/main/java/ru/job4j/qwartz/AlertRabbit.java
@@ -3,12 +3,15 @@ package ru.job4j.qwartz;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Properties;
+import java.util.TimeZone;
 
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
@@ -16,35 +19,86 @@ import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
 
+    private static Properties getProperties() {
+        Properties properties = new Properties();
+        try (InputStream in = Rabbit.class.getClassLoader()
+                .getResourceAsStream("rabbit.properties")) {
+            properties.load(in);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return properties;
+    }
+
+    private static Connection getConnection() {
+        Properties config = getProperties();
+        Connection connection;
+        try {
+            Class.forName(config.getProperty("driver-class-name"));
+            connection = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password"));
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        return connection;
+    }
+
     public static void main(String[] args) {
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(RabbitProperties.class).build();
+            JobDataMap data = new JobDataMap();
+            int interval = Integer.parseInt(getProperties()
+                    .getProperty("rabbit.interval"));
+            data.put("interval", interval);
+            data.put("connection", getConnection());
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(11)
-                    .withRepeatCount(3);
+                    .withIntervalInSeconds(interval)
+                    .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
+            Thread.sleep(10000);
+            scheduler.shutdown();
+        } catch (Exception se) {
             se.printStackTrace();
         }
     }
 
-    public static class RabbitProperties implements Job {
+    public static class Rabbit implements Job {
+
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
+
         @Override
         public void execute(JobExecutionContext context) {
-            File file = new File("rabbit.properties");
-            List<String> result = new ArrayList<>();
-            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-                br.lines().forEach(result::add);
-            } catch (IOException e) {
+            System.out.println("Rabbit runs here...");
+            try (PreparedStatement ps = ((Connection) context
+                    .getJobDetail()
+                    .getJobDataMap()
+                    .get("connection")).
+                    prepareStatement(
+                            "insert into rabbit (created_date) values (?)")) {
+                LocalDateTime jobTime = LocalDateTime
+                        .ofInstant(Instant.ofEpochMilli(context.getFireTime().getTime()),
+                                TimeZone.getDefault().toZoneId());
+                ps.setTimestamp(1, Timestamp.valueOf(jobTime));
+                System.out.printf("Rabbit runs here with frequency: %s seconds",
+                        context.getJobDetail()
+                                .getJobDataMap()
+                                .get("interval"));
+                ps.execute();
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-            System.out.println(result);
+        }
     }
-}
 }
